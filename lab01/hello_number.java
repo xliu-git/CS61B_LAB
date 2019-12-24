562585198055a391f641008033a325aeaa373de3
Xiaohan Liu <xliu194@syr.edu>
2019-12-24 12:56:35 -0500

    add hello_world.java and number_sum.java

---
+++ lab01/hello_number.java
@@ -0,0 +1,9 @@
+public class hello_number{
+  public static void main(String[] args){
+       int x=0;
+        while (x<10){
+           System.out.print(x + " ");
+           x=x+1;
+}
+}
+}
---
+++ lab01/number_sum.java
@@ -0,0 +1,14 @@
+
+
+public class number_sum{
+  public static void main(String[] args){
+       int x=0;
+int sum=0;
+        while (x<10){
+           System.out.print(sum + " ");
+
+           x=x+1;
+sum=sum+x;
+}
+}
+}
